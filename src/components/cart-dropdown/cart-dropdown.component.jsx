66acb6490838b1efd69d9d59a88bb1b304ed46dc
Thailik <thailik@Thais-MacBook-Pro-2.local>
2024-04-22 23:07:47 -0400

    migrating to redux (done)

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,22 +1,27 @@
 import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-
-import { selectCartItems } from '../../store/cart/cart.selector';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectCartItems, selectIsCartOpen } from '../../store/cart/cart.selector';
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
-
 import {
   CartDropdownContainer,
   EmptyMessage,
   CartItems,
 } from './cart-dropdown.styles';
+import { setIsCartOpen } from '../../store/cart/cart.reducer';
+
+
 
 const CartDropdown = () => {
+  const dispatch = useDispatch();
+  const isCartOpen = useSelector(selectIsCartOpen);
   const cartItems = useSelector(selectCartItems);
+  const toggleIsCartOpen = () => dispatch(setIsCartOpen(!isCartOpen));
   const navigate = useNavigate();
 
   const goToCheckoutHandler = () => {
     navigate('/checkout');
+    toggleIsCartOpen()
   };
 
   return (
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -21,12 +21,12 @@ import {
 } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+  apiKey: "AIzaSyCkwSKYsnUQyI34JKFFw45XEJ7z7TeCPRg",
+  authDomain: "e-shop-db-28e3c.firebaseapp.com",
+  projectId: "e-shop-db-28e3c",
+  storageBucket: "e-shop-db-28e3c.appspot.com",
+  messagingSenderId: "677810627061",
+  appId: "1:677810627061:web:e7340c9f3e2a911df380bc"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
